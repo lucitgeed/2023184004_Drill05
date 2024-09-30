c78635942eda03497d949fd3301da6ef1e53561f
homedesktop@2DGP <gunageroo@gmail.com>
2024-09-30 15:02:14 +0900

    esc키 종료 구현

--- Drill05_char_move.py
+++ Drill05_char_move.py
@@ -16,10 +16,23 @@ for x in range(0, 800, 10):
 
 
 
+running = True
+
+def handle_events():
+    global running
+
+    events = get_events()
+    for event in events:
+        if event.type == SDL_QUIT:
+            running = False
+        elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
+            running = False
 
 
 
 
-    
+
+
+
 
 close_canvas()
