fbae3ba0024512541a379d2d971c88951a6a4c9d
homedesktop@2DGP <gunageroo@gmail.com>
2024-09-30 14:49:44 +0900

    말 달리기 구현

--- Drill05_char_move.py
+++ Drill05_char_move.py
@@ -2,14 +2,14 @@ from pico2d import*
 
 open_canvas()
 
-horse = load_image('horse2.png')
+horse = load_image('horse4.png')
 
 frame = 0
 for x in range(0, 800, 10):
     clear_canvas()
-    horse.clip_draw(frame*10, 0, 200, 150, x, 90)
+    horse.clip_draw(frame* 223, 0, 223, 160, x, 90)
     update_canvas()
-    frame = (frame + 1) & 8
+    frame = (frame + 1) % 7
     delay(0.05)
 
 close_canvas()
