548a9456e024777af9aac7a53a471c04dd774e39
homedesktop@2DGP <gunageroo@gmail.com>
2024-09-30 14:56:36 +0900

    배경 추가

--- Drill05_char_move.py
+++ Drill05_char_move.py
@@ -1,15 +1,25 @@
 from pico2d import*
 
 open_canvas()
-
+tuk = load_image("TUK_GROUND.png")
 horse = load_image('horse4.png')
 
 frame = 0
 for x in range(0, 800, 10):
     clear_canvas()
+    tuk.draw(350, 300)
     horse.clip_draw(frame* 223, 0, 223, 160, x, 90)
     update_canvas()
     frame = (frame + 1) % 7
     delay(0.05)
+
+
+
+
+
+
+
+
+    
 
 close_canvas()
